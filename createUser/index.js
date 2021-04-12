ca025d3e240d5df142509967e263d63894085bdb
Christian Frølich <chfr20ab@student.cbs.dk>
2021-04-12 14:34:13 +0200

    test

--- createUser/index.js
+++ createUser/index.js
@@ -1,10 +1,21 @@
+const db = require('/../shared/db')
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
+    const birthday = (req.query.birthday || (req.body && req.body.birthday));
+    const email = (req.query.email || (req.body && req.body.email));
+    const gender = (req.query.gender || (req.body && req.body.gender));
+    const country = (req.query.country || (req.body && req.body.country));
+    //const hashed = (req.query.hashed_password || (req.body && req.body.hashed_password));
+
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ?  name + " " + birthday + " " + email + " " + gender + " " + " "+ country + " " + "orale"
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+        const sql_stmt = `insert into users (name, birthday, email, gender, country)
+        values ('Christian','1999-06-28','123j@gmail.com','male', 'den')`
 
     context.res = {
         // status: 200, /* Defaults to 200 */
@@ -13,9 +24,5 @@ module.exports = async function (context, req) {
 }
 
 
-<<<<<<< HEAD
-=======
-// test push arved
 
-//test løkken
->>>>>>> b8a0551e427c608f5f9ae68112a565e5f7cdcff2
+
